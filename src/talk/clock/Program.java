ec409fe2ed714ffe827834f5a4a2d6f354df6302
Benjamin Ifie <ben.ifie@wonga.com>
2012-10-25 18:18:29 +0100

    program

--- src/talk/clock/Program.java
+++ src/talk/clock/Program.java
@@ -1,5 +1,11 @@
 package talk.clock;
 
 public class Program {
+	
+	public static void main(String [ ] args)
+	{
+		ConverseClock clock = new ConverseClock();
+		System.out.println(clock.theTimeIs());
+	}
 
 }
